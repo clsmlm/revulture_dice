16c64c939f52c4acd906879b93f44f070c31983c
clsm <r.k.s.gk.m.tj@gmail.com>
2024-04-08 17:30:11 +0900

    add

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Form from './Form';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Form />
+    <Router basename={process.env.PUBLIC_URL}>
+      <Form />
+    </Router>
   </React.StrictMode>
 );
 
